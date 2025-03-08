7858276f83897a2caca8c08271885254c01930ef
YaseenAmin21 <yaseenamindev@gmail.com>
2025-03-08 17:19:15 -0600

    Linking a JavaScript File

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,34 @@
+// ###########  006 Section Intro ##########
+// ###########  007 Hello World! ##########
+// ###########  008 A Brief Introduction to JavaScript ##########
+// ###########  009 Linking a JavaScript File ##########
+
+let js = "amazing";
+if (js === "amazing") {
+  alert("JavaScript is FUN!");
+}
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+// ###########  010 Values and Variables ##########
+// ###########  011 assignments-js-fundamentals ##########
+// ###########  012 Data Types ##########
+// ###########  013 let, const and var ##########
+// ###########  014 Basic Operators ##########
+// ###########  015 Operator Precedence ##########
+// ###########  016 Coding Challenge #1 ##########
+// ###########  017 Strings and Template Literals ##########
+// ###########  018 Taking Decisions_ if _ else Statements ##########
+// ###########  019 Coding Challenge #2 ##########
+// ###########  020 Type Conversion and Coercion ##########
+// ###########  021 Truthy and Falsy Values ##########
+// ###########  022 Equality Operators_ == vs. === ##########
+// ###########  023 Boolean Logic ##########
+// ###########  024 Logical Operators ##########
+// ###########  025 Coding Challenge #3 ##########
+// ###########  026 The switch Statement ##########
+// ###########  027 Statements and Expressions ##########
+// ###########  028 The Conditional (Ternary) Operator ##########
+// ###########  029 Coding Challenge #4 ##########
+// ###########  030 JavaScript Releases_ ES5, ES6+ and ESNext ##########
