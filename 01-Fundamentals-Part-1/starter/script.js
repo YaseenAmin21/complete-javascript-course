b8ca99c90488d2a1d3e4a628d285babc72c8772f
YaseenAmin21 <yaseenamindev@gmail.com>
2025-03-08 18:34:17 -0600

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,15 +3,64 @@
 // ###########  008 A Brief Introduction to JavaScript ##########
 // ###########  009 Linking a JavaScript File ##########
 
-let js = "amazing";
-if (js === "amazing") {
-  alert("JavaScript is FUN!");
-}
+// let js = "amazing";
+// if (js === "amazing") {
+//   alert("JavaScript is FUN!");
+// }
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
 
 // ###########  010 Values and Variables ##########
+
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Yaseen");
+console.log(23);
+
+// camelCase
+// PascalCase
+// UPPER_SNAKE_CASE
+// _prefixWithUnderscore
+// #privateField
+// is/has/can
+// kebab-case
+
+let firtName = "Yaseen";
+let first = "Yaseen";
+let firstNamePerson = "Yaseen";
+let first_name_person = "Yaseen";
+
+// ------ illegal way of naming a variable ------
+
+// let 3year = 3
+// let yaseen&Amin = 'AM'
+// let new = 27
+
+console.log(firtName);
+console.log(firtName);
+console.log(firtName);
+
+let yaseen_amin = "Yaseen";
+// let yaseen&Amin = 'AM'
+let $function = "Yaseen";
+// let new = 27
+let name = "Yaseen";
+
+// Start with lower case
+let person = "Yaseeen";
+// Constants
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
+
 // ###########  011 assignments-js-fundamentals ##########
 // ###########  012 Data Types ##########
 // ###########  013 let, const and var ##########
